713987880afc39037db5a42015fc206d4460426e
ARYAN TIWARI <96858602+aryantiwari009@users.noreply.github.com>
2026-06-21 23:28:45 +0530

    Implimented logger

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./handlers/logger";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
@@ -9,39 +11,45 @@ if (!MONGODB_URI) {
 interface MongooseCache {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
-}   
+}
 
 declare global {
-  var mongooseCache: MongooseCache;
+
+  var mongoose: MongooseCache;
 }
 
-let cached = global.mongooseCache;
+let cached = global.mongoose;
 
-if(!cached){
-    cached = global.mongooseCache = {
-        conn: null,
-        promise: null
-    };
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-const dbconnect = async (): Promise<Mongoose> => {
-    if(cached.conn){
-        return cached.conn;
-    }
-    if(!cached.promise){
-        cached.promise = mongoose.connect(MONGODB_URI, {
-            dbName: "devflow",
-        }).then((result) => {
-            console.log("Connected to MongoDB");
-            return result;
-        }).catch((error) => {
-            console.error("Error connecting to MongoDB:", error);
-            throw error;
-        });
-    }
-
-    cached.conn = await cached.promise;
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    logger.info("Using existing mongoose connection");
     return cached.conn;
-}
+  }
 
-export default dbconnect;
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        console.log("Connected to MongoDB");
+        logger.info("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB", error);
+        logger.error("Error connecting to MongoDB", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
+
+export default dbConnect;
