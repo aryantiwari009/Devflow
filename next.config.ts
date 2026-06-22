94174a1bf3d33a4b86425d2b648d82775cbefdcb
ARYAN TIWARI <96858602+aryantiwari009@users.noreply.github.com>
2026-06-22 13:17:26 +0530

    Add serverExternalPackages configuration for pino and pino-pretty

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['pino', 'pino-pretty'], // Add the packages you want to use in the server environment
   reactCompiler: true, // This requires the package you just installed
   // ... other config
   allowedDevOrigins: ["192.168.31.125"],
